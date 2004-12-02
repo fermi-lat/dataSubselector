857411230213357da3f53d1d22ad42aacf9a506a
jchiang <>
2004-12-02 18:26:42 +0000

    initial working version of Cuts class; add test code

--- dataSubselector/Cuts.h
+++ dataSubselector/Cuts.h
@@ -1,13 +1,17 @@
 /**
  * @file Cuts.h
- * @brief Handle data selections and DSS keywords in a portable way.
+ * @brief Handle data selections and DSS keywords for packages outside of
+ * dataSubselector.
  * @author J. Chiang
  *
- * $Header$
+ * $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/dataSubselector/Cuts.h,v 1.2 2004/12/02 06:12:03 jchiang Exp $
  */
 
-#ifndef dataSubelector_Cuts_h
-#define dataSubelector_Cuts_h
+#ifndef dataSubselector_Cuts_h
+#define dataSubselector_Cuts_h
+
+#include <string>
+#include <vector>
 
 #include "tip/Header.h"
 #include "tip/Table.h"
@@ -18,30 +22,38 @@ namespace dataSubselector {
 
 /**
  * @class Cuts
+ * @brief Handle data selections and DSS keywords for packages outside of
+ * dataSubselector.
+ * @author J. Chiang
  *
+ * $Header$
  */
 
 class Cuts {
 
+   class CutBase;
+
 public: 
 
-   Cuts(tip::Header & header);
+   Cuts() {}
 
    ~Cuts();
 
    bool accept(tip::ConstTableRecord & row) const;
 
    unsigned int addRangeCut(const std::string & keyword, 
+                            const std::string & unit,
                             double minVal, double maxVal);
 
    unsigned int addTableCut(const std::string & keyword,
-                            tip::ConstTableRecord & table);
+                            const std::string & unit,
+                            const tip::Table & table);
 
    unsigned int addSkyConeCut(double ra, double dec, double radius);
 
-private:
+   void writeDssKeywords(tip::Header & header) const;
 
-   tip::Header & m_header;
+private:
 
    std::vector<CutBase *> m_cuts;
    
@@ -50,32 +62,43 @@ private:
       CutBase() {}
       virtual ~CutBase() {}
       virtual bool accept(tip::ConstTableRecord & row) const = 0;
-      virtual void writeDssKeywords(tip::Header & header) const = 0;
+      virtual void writeDssKeywords(tip::Header & header, 
+                                    unsigned int keynum) const = 0;
+   protected:
+      void writeDssKeywords(tip::Header & header, unsigned int keynum,
+                            const std::string & type, const std::string & unit,
+                            const std::string & value) const;
    };
 
    class RangeCut : public CutBase {
    public:
-      RangeCut(const std::string & keyword, double minVal, double maxVal) :
-         m_keyword(keyword), m_min(minVal), m_max(maxVal) {}
+      RangeCut(const std::string & keyword, const std::string & unit,
+               double minVal, double maxVal) :
+         m_keyword(keyword), m_unit(unit), m_min(minVal), m_max(maxVal) {}
       virtual ~RangeCut() {}
       virtual bool accept(tip::ConstTableRecord & row) const;
-      virtual void writeDssKeywords(tip::Header & header) const;
+      virtual void writeDssKeywords(tip::Header & header, 
+                                    unsigned int keynum) const;
    private:
       std::string m_keyword;
+      std::string m_unit;
       double m_min;
       double m_max;
    };
 
    class TableCut : public CutBase {
    public:
-      TableCut(const std::string & keyword, tip::ConstTableRecord & table) :
-         m_keyword(keyword), m_table(table) {}
+      TableCut(const std::string & keyword, const std::string & unit,
+               const tip::Table & table) :
+         m_keyword(keyword), m_unit(unit), m_table(table) {}
       virtual ~TableCut() {}
       virtual bool accept(tip::ConstTableRecord & row) const;
-      virtual void writeDssKeywords(tip::Header & header) const;
+      virtual void writeDssKeywords(tip::Header & header, 
+                                    unsigned int keynum) const;
    private:
       std::string m_keyword;
-      tip::ConstTableRecord & m_table;
+      std::string m_unit;
+      const tip::Table & m_table;
    };
 
    class SkyConeCut : public CutBase {
@@ -86,7 +109,8 @@ private:
          m_coneCenter(dir), m_radius(radius) {}
       virtual ~SkyConeCut() {}
       virtual bool accept(tip::ConstTableRecord & row) const;
-      virtual void writeDssKeywords(tip::Header & header) const;
+      virtual void writeDssKeywords(tip::Header & header, 
+                                    unsigned int keynum) const;
    private:
       astro::SkyDir m_coneCenter;
       double m_radius;
@@ -94,4 +118,4 @@ private:
 };
 } // namespace dataSubselector
 
-#endif dataSubelector_Cuts_h
+#endif // dataSubselector_Cuts_h
--- src/Cuts.cxx
+++ src/Cuts.cxx
@@ -1,21 +1,19 @@
 /**
  * @file Cuts.cxx
- * @brief Handel data selections and DSS keyword management.
+ * @brief Handle data selections and DSS keyword management.
  * @author J. Chiang
  *
- * $Header$
+ * $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/src/Cuts.cxx,v 1.1 2004/12/02 06:12:03 jchiang Exp $
  */
+
+#include <sstream>
 
 #include "dataSubselector/Cuts.h"
 
-//namespace dataSubselector {
-
-Cuts::Cuts(tip::Header & header) : m_header(header) {
-}
+namespace dataSubselector {
 
 Cuts::~Cuts() {
    for (unsigned int i = 0; i < m_cuts.size(); i++) {
-      m_cuts[i]->writeDssKeywords(m_header);
       delete m_cuts[i];
    }
 }
@@ -29,20 +27,41 @@ bool Cuts::accept(tip::ConstTableRecord & row) const {
 }
 
 unsigned int Cuts::addRangeCut(const std::string & keyword,
-                         double minVal, double maxVal) {
-   m_cuts.append(new Cuts::RangeCut(keyword, minVal, maxVal));
+                               const std::string & unit,
+                               double minVal, double maxVal) {
+   m_cuts.push_back(new Cuts::RangeCut(keyword, unit, minVal, maxVal));
    return m_cuts.size();
 }
 
 unsigned int Cuts::addTableCut(const std::string & keyword,
-                         tip::ConstTableRecord & table) {
-   m_cuts.append(new Cuts::TableCut(keyword, table));
+                               const std::string & unit,
+                               const tip::Table & table) {
+   m_cuts.push_back(new Cuts::TableCut(keyword, unit, table));
    return m_cuts.size();
 }
 
 unsigned int Cuts::addSkyConeCut(double ra, double dec, double radius) {
-   m_cuts.append(new Cuts::SkyConeCut(ra, dec, radius));
+   m_cuts.push_back(new Cuts::SkyConeCut(ra, dec, radius));
    return m_cuts.size();
+}
+
+void Cuts::writeDssKeywords(tip::Header & header) const {
+   for (unsigned int i = 0; i < m_cuts.size(); i++) {
+      m_cuts[i]->writeDssKeywords(header, i + 1);
+   }
+}      
+
+void Cuts::CutBase::writeDssKeywords(tip::Header & header, unsigned int keynum,
+                                     const std::string & type, 
+                                     const std::string & unit,
+                                     const std::string & value) const {
+   std::ostringstream key1, key2, key3;
+   key1 << "DSTYP" << keynum;
+   header[key1.str()].set(type);
+   key2 << "DSUNI" << keynum;
+   header[key2.str()].set(unit);
+   key3 << "DSVAL" << keynum;
+   header[key3.str()].set(value);
 }
 
 bool Cuts::RangeCut::accept(tip::ConstTableRecord & row) const {
@@ -53,7 +72,9 @@ bool Cuts::RangeCut::accept(tip::ConstTableRecord & row) const {
 
 void Cuts::RangeCut::writeDssKeywords(tip::Header & header, 
                                       unsigned int keynum) const {
-   std::ostringstream 
+   std::ostringstream value;
+   value << m_min << ":" << m_max;
+   CutBase::writeDssKeywords(header, keynum, m_keyword, m_unit, value.str());
 }
 
 bool Cuts::TableCut::accept(tip::ConstTableRecord & row) const {
@@ -61,6 +82,14 @@ bool Cuts::TableCut::accept(tip::ConstTableRecord & row) const {
    row[m_keyword].get(value);
 /// @todo implement this
    return true;
+}
+
+void Cuts::TableCut::writeDssKeywords(tip::Header & header,
+                                      unsigned int keynum) const {
+   const tip::Header & table_header = m_table.getHeader();
+   std::string table_name;
+   table_header["EXTNAME"].get(table_name);
+   CutBase::writeDssKeywords(header, keynum, m_keyword, m_unit, table_name);
 }
 
 bool Cuts::SkyConeCut::accept(tip::ConstTableRecord & row) const {
@@ -71,5 +100,15 @@ bool Cuts::SkyConeCut::accept(tip::ConstTableRecord & row) const {
    return separation*180./M_PI <= m_radius;
 }
 
-} // namespace dataSubselector
+void Cuts::SkyConeCut::writeDssKeywords(tip::Header & header,
+                                        unsigned int keynum) const {
+   std::ostringstream value;
+   value << "CIRCLE(" 
+         << m_coneCenter.ra() << "," 
+         << m_coneCenter.dec() << ","
+         << m_radius << ")";
+   CutBase::writeDssKeywords(header, keynum, "POS(RA,DEC)", "deg", 
+                             value.str());
+}
 
+} // namespace dataSubselector

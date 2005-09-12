6c7ba62a4c64627ca2f637bee94e5014e48c2000
jchiang <>
2005-09-12 17:35:16 +0000

    add check for input FT1 file

--- src/dataSubselector/dataSubselector.cxx
+++ src/dataSubselector/dataSubselector.cxx
@@ -3,7 +3,7 @@
  * @brief Filter FT1 data.
  * @author J. Chiang
  *
- *  $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/src/dataSubselector/dataSubselector.cxx,v 1.18 2005/07/01 22:32:56 jchiang Exp $
+ *  $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/src/dataSubselector/dataSubselector.cxx,v 1.19 2005/07/13 18:52:13 jchiang Exp $
  */
 
 #include "facilities/Util.h"
@@ -27,7 +27,7 @@ using dataSubselector::CutController;
  * @class DataFilter
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/src/dataSubselector/dataSubselector.cxx,v 1.18 2005/07/01 22:32:56 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/dataSubselector/src/dataSubselector/dataSubselector.cxx,v 1.19 2005/07/13 18:52:13 jchiang Exp $
  */
 
 class DataFilter : public st_app::StApp {
@@ -90,6 +90,7 @@ void DataFilter::run() {
    std::string inputFile = m_pars["infile"];
    m_inputFile = inputFile;
    facilities::Util::expandEnvVar(&m_inputFile);
+   st_facilities::Util::file_ok(m_inputFile);
 
    std::string outputFile = m_pars["outfile"];
    m_outputFile = outputFile;

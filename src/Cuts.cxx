14b7b3713a1d040574d7af9a816a052eb542a5db
jchiang <>
2012-11-16 20:36:18 +0000

    back off on irf checking until non-standard irfs are handled correctly

--- src/Cuts.cxx
+++ src/Cuts.cxx
@@ -3,7 +3,7 @@
  * @brief Handle data selections and DSS keyword management.
  * @author J. Chiang
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/dataSubselector/src/Cuts.cxx,v 1.52 2012/10/16 20:26:56 jchiang Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/ScienceTools-scons/dataSubselector/src/Cuts.cxx,v 1.53 2012/11/11 03:24:11 jchiang Exp $
  */
 
 #include <cctype>
@@ -475,7 +475,7 @@ void Cuts::checkIrfs(const std::string & infile,
    my_cuts.set_irfName(infile, extname);
    if (my_cuts.irfName() != irfs) {
       st_stream::StreamFormatter formatter("dataSubselector::Cuts",
-                                           "checkIrfs", 2);
+                                           "checkIrfs", 4);
       formatter.warn() << "IRF selection, "
                        << irfs << ", and DSS keywords in "
                        << infile << "[" << extname << "]\n"
@@ -609,13 +609,13 @@ read_bitmask_mapping(std::map<unsigned int, std::string> & irfs) const {
       }
    }
    delete irf_map;
-   if (irfs.size() == 0 && m_pass_ver != "NONE") {
-      std::ostringstream message;
-      message << "dataSubselector::Cuts::read_bitmask_mapping: "
-              << "PASS_VER not found in irf_index.fits: "
-              << m_pass_ver;
-      throw std::runtime_error(message.str());
-   }
+//    if (irfs.size() == 0 && m_pass_ver != "NONE") {
+//       std::ostringstream message;
+//       message << "dataSubselector::Cuts::read_bitmask_mapping: "
+//               << "PASS_VER not found in irf_index.fits: "
+//               << m_pass_ver;
+//       throw std::runtime_error(message.str());
+//    }
 }
 
 void Cuts::read_pass_ver(const std::string & infile, 
